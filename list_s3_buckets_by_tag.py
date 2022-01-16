bd5aec7330eb703819acf98a18ae42b68c8274d9
luk-kop <luk.kopicki@gmail.com>
2022-01-16 21:17:40 +0100

    Added python script that displays the S3 bucket names by tag.

---
+++ list_s3_buckets_by_tag.py
@@ -0,0 +1,38 @@
+import argparse
+
+from s3_orphan import get_buckets
+
+
+def get_bucket_names(tag_key: str, tag_value: str) -> list:
+    """
+    Returns the list of S3 bucket names with the relevant tags.
+    """
+    buckets_data = get_buckets(tag_key=tag_key, tag_value=tag_value)
+    if not buckets_data:
+        return []
+    return [bucket['name'] for bucket in buckets_data]
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Displays a list of S3 buckets with the specified tag'
+    )
+    # Arguments
+    parser.add_argument('-k',
+                        '--tag-key',
+                        type=str,
+                        required=True,
+                        help='list S3 bucket names with specified tag key')
+    parser.add_argument('-v',
+                        '--tag-value',
+                        type=str,
+                        required=True,
+                        help='list S3 bucket names with specified tag value')
+    # Parse arguments
+    args = parser.parse_args()
+    tag_key_argument = args.tag_key
+    tag_value_argument = args.tag_value
+
+    # Run script's main func
+    bucket_names = get_bucket_names(tag_key=tag_key_argument, tag_value=tag_value_argument)
+    print(bucket_names)
